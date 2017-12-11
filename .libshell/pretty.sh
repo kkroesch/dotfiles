e96a8a88f8810781e0cadb3398e58ce134ed373c
Karsten Kroesch <kkroesch@icloud.com>
2017-12-11 10:56:29 +0100

    Reorganized config files.

---
+++ .libshell/pretty.sh
@@ -0,0 +1,28 @@
+# Functions for colorized console output.
+
+function header {
+  # Colored output
+  echo ""
+  echo "$(tput setaf 6)$1$(tput sgr0)"
+  echo ""
+}
+
+function box_out() {
+  # First Argument is the title of the box
+	title=$1
+	shift
+  local s=("$@") b w
+  for l in "${s[@]}"; do
+    ((w<${#l})) && { b="$l"; w="${#l}"; }
+  done
+  tput setaf 3
+  h=${b:${#title}}
+  echo "┌─$title${h//?/─}─┐
+│ ${b//?/ } │"
+  for l in "${s[@]}"; do
+    printf '│ %s%*s%s │\n' "$(tput setaf 4)" "-$w" "$l" "$(tput setaf 3)"
+  done
+  echo "│ ${b//?/ } │
+└─${b//?/─}─┘"
+  tput sgr 0
+}
---
+++ .libshell/ssh.sh
@@ -0,0 +1,24 @@
+# SSH Agent Tools
+
+jump () {
+	# Forward agent to jump host
+	HOST=$1 
+	ssh -At $JUMPHOST ssh -At $HOST
+}
+
+export SESSION_FILE=~/.ssh/session
+
+function sshagent() {
+  # Decrypt SSH key and save agent session
+  eval `ssh-agent` && ssh-add .ssh/id_ecdsa
+  echo export SSH_AUTH_SOCK=$SSH_AUTH_SOCK > $SESSION_FILE
+  echo export SSH_AGENT_PID=$SSH_AGENT_PID >> $SESSION_FILE
+  chmod 600 $SESSION_FILE
+}
+
+function ssh-reconnect() {
+  # Reconnect agent on new terminal session
+  source $SESSION_FILE
+}
+[ -f $SESSION_FILE ] && ssh-reconnect
+
