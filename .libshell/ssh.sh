bebedae727ba8f461650e99ca8331ddcb766eb84
Karsten Kroesch <kkroesch@me.com>
2018-11-28 11:21:38 +0100

    Fixed SSH key path.

--- .libshell/ssh.sh
+++ .libshell/ssh.sh
@@ -16,7 +16,7 @@ export SESSION_FILE=~/.ssh/session
 
 function sshagent() {
   # Decrypt SSH key and save agent session
-  eval `ssh-agent` && ssh-add .ssh/id_ecdsa
+  eval `ssh-agent` && ssh-add ~/.ssh/id_ecdsa
   echo export SSH_AUTH_SOCK=$SSH_AUTH_SOCK > $SESSION_FILE
   echo export SSH_AGENT_PID=$SSH_AGENT_PID >> $SESSION_FILE
   chmod 600 $SESSION_FILE
