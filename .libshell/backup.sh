6e0715fa415aa370e77236e03caaa81ed8e1032f
Kroesch Karsten, INI-ONE-SCC-VMI <Karsten.Kroesch@swisscom.com>
2019-08-06 08:44:50 +0200

    Password management.

--- .libshell/backup.sh
+++ .libshell/backup.sh
@@ -1,5 +1,6 @@
 export RESTIC_REPOSITORY=${HOME}/myCloud/backup
 export RESTIC_PASSWORD_FILE=${HOME}/.resticpassword
+#export RESTIC_PASSWORD=$(pass restic)
 
 function backup() {
     restic backup --exclude-file="${HOME}/.libshell/backup-exclude.txt" $1
