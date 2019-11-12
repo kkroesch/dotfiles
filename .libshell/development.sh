3e4a073e814d3020309be9dec822ef0995c440dd
Kroesch Karsten, INI-ONE-SCC-VMI <Karsten.Kroesch@swisscom.com>
2019-11-12 08:34:28 +0100

    Added development tools

--- .libshell/development.sh
+++ .libshell/development.sh
@@ -1,5 +1,15 @@
+# Tools for Git repositories
 
 function clone() {
   # Clone a repository from Github
   git clone https://github.com/$1 
 }
+
+function addremotes() {
+  # Add two remotes to a local repository (primary and backup)
+  REPO_NAME=$(basename `git rev-parse --show-toplevel`)
+  git remote add origin git@github.com:kkroesch/${REPO_NAME}.git
+  git remote add backup git@nas2.home:${REPO_NAME}.git
+}
+
+
