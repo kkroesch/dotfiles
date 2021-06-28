d1514a44a6190748d73df87cf99c70f4d313720f
Karsten Kroesch <Karsten.Kroesch@swisscom.com>
2021-06-28 12:52:51 +0200

    Added Wezterm scrollbar.

--- .config/wezterm.lua
+++ .config/wezterm.lua
@@ -5,12 +5,15 @@ return {
     color_scheme = "Solarized Dark - Patched",
     default_prog = {"wsl.exe"},
     default_cwd = "/Users/kkr/Projects",
+    enable_scroll_bar = true,
     launch_menu = {
         {
           label = "WSL ZSH",
-          -- The argument array to spawn.  If omitted the default program
-          -- will be used as described in the documentation above
           args = {"wsl", ""},
+        },
+        {
+          label = "Powershell",
+          args = {"powershell.exe", ""},
         },
     }
 }
