897c7eaceba0518fee1ba35ed5a6f604f72022be
Karsten Kroesch <karsten.kroesch@ag.ch>
2023-12-30 17:23:33 +0100

    Added Python stuff

--- .libshell/python.sh
+++ .libshell/python.sh
@@ -4,6 +4,8 @@ alias activate="source .venv/bin/activate"
 alias venv="python3.6 -m venv .venv && source .venv/bin/activate"
 alias dj="./manage.py"
 
+alias pyignore="curl -lso .gitignore -L https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore"
+
 function pyinit() {
   # Make a Python project from current directory including virtual environment and Git repository
 	mkenv
