372d9262a88eea5b5d52b14bf55b06d2a5e72827
Karsten Kroesch <karsten.kroesch@ag.ch>
2024-05-17 09:00:23 +0200

    Migratign to Neovim.

---
+++ .config/nvim/lua/config.lua
@@ -0,0 +1,54 @@
+
+-- Lualine Configuration
+require('lualine').setup {
+  options = {
+    theme = 'gruvbox'
+  }
+}
+
+-- Telescope Configuration
+require('telescope').setup {}
+
+-- Autocompletion Configuration
+local cmp = require'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+    { name = 'path' },
+  },
+}
+
+--Nvim Tree
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+
--- .zshrc
+++ .zshrc
@@ -116,7 +116,7 @@ export GPG_TTY=$(tty)
 eval "$(zoxide init --cmd cd zsh)"
 
 # Enable Fuzzy Finder (fzf) for searching command line history:
-source /usr/share/fzf/shell/key-bindings.zsh
+source /usr/share/fzf/key-bindings.zsh
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 bindkey '^R' fzf-history-widget
 
