acd2e47a4efb3efe3350de7467528b0e9d63d452
Karsten Kroesch <karsten.kroesch@ag.ch>
2024-07-01 10:08:37 +0200

    Refactoring.

--- .libshell/common.sh
+++ .libshell/common.sh
@@ -19,3 +19,30 @@ function fatal() {
     exit 1
 }
 
+#
+# Crypto utils
+#
+
+alias mkpassword="openssl rand -base64 12"
+alias mkpassphrase="gpg --gen-random --armor 0 24"
+
+function encrypt() {
+  # Encrypt file
+  INFILE=$1
+  OUTFILE=$1.enc
+  openssl enc -aes-256-cbc -a -salt -in $INFILE -out $OUTFILE
+}
+
+function decrypt() {
+  # Decrypt file
+  openssl enc -d -aes-256-cbc -a -in $1
+}
+
+#
+# Date Utilities
+#
+
+function ts2date {
+  date -d @$1 '+%d.%m.%Y %H:%M:%S'
+}
+
--- .libshell/date.sh
+++
@@ -1,3 +0,0 @@
-function ts2date {
-  date -d @$1 '+%d.%m.%Y %H:%M:%S'
-}
